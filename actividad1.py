0a370083129aa286a5a1bd0bc9ff7070339c37d0
Yilberth <yilberthgalarza444@gmail.com>
2021-09-04 15:38:20 -0500

    Comentario

---
+++ actividad1.py
@@ -0,0 +1,28 @@
+from library.mod_numero_perfecto import *
+
+def opciones():
+    op = 0
+    while(op < 1 or op > 4):
+
+        op = int(input("Que opcion elijes: "))
+
+        if(op < 1 or op > 4):
+            print("Por favor elija una opcion entre 1 y 4")
+        
+    if(op == 1):
+        punto1()
+    if(op == 2):
+        punto2()
+    if(op == 3):
+        punto3()
+    if(op == 4):
+        punto4()
+
+
+
+def inicio():
+    opciones()
+ 
+
+if __name__== '__main__':
+    inicio()
---
+++ library/mod_numero_perfecto.py
@@ -0,0 +1,15 @@
+        #Punto 3
+def punto3():
+    numero = int(input("Escriba cualquier numero para determinar si es numero perfecto: "));
+
+    i = 1
+    comprobar = 0;
+    while(i < numero):
+        if(numero%i == 0):
+            comprobar = comprobar + i
+        i = i +1
+
+    if(comprobar == numero):
+        return print("Este numero es perfecto!!")
+    else:
+        return print("Este numero no es perfecto!!")
