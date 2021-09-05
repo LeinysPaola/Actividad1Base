08738ba119b993e04809883257330a7b12c7bcc7
Yilberth <yilberthgalarza444@gmail.com>
2021-09-04 20:34:12 -0500

    comentario

--- library/mod_opciones.py
+++ library/mod_opciones.py
@@ -1,17 +1,1 @@
-from mod_numero_perfecto import *
-from mod_funcion_exponencial import *
 
-def opciones():
-    op = 0
-    while(op < 1 or op > 4):
-
-        op = int(input("Que opcion elijes: "))
-
-        if(op < 1 or op > 4):
-            print("Por favor elija una opcion entre 1 y 4")
-        
- 
-    if(op == 3):
-        punto3()
-    if(op == 4):
-        punto4()
