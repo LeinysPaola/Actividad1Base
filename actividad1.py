02a48bd66ea5efca06ed0a4113c89e9ea5a48a7e
Yilberth <yilberthgalarza444@gmail.com>
2021-09-04 21:03:07 -0500

    yilberth

--- actividad1.py
+++ actividad1.py
@@ -1,20 +1,4 @@
-from mod_numero_perfecto import *
-from mod_funcion_exponencial import *
-
-def opciones():
-    op = 0
-    while(op < 1 or op > 4):
-
-        op = int(input("Que opcion elijes: "))
-
-        if(op < 1 or op > 4):
-            print("Por favor elija una opcion entre 1 y 4")
-        
- 
-    if(op == 3):
-        punto3()
-    if(op == 4):
-        punto4()
+from library.mod_opciones import *
 
 def inicio():
     opciones()
