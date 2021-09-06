61daaf7c97b37eb15ab41257369d5badf6280ae7
LeinysPaola <lepagifo@hotmail.com>
2021-09-05 22:33:03 -0500

    leinys

--- library/mod_numero_par_o_impar.py
+++ library/mod_numero_par_o_impar.py
@@ -1,8 +1,8 @@
 # punto1 
   
-def punto1 ():
+def punto1():
     numero = int (input("digite un numero para saber si es par o impar: ")) 
-    print(numero)
+    
     if ( numero%2 == 0): 
     
         return print("Este numero es par!")
--- library/mod_opciones.py
+++ library/mod_opciones.py
@@ -1,7 +1,7 @@
 from library.mod_numero_primo import *
 from library.mod_funcion_exponencial import *
 from library.mod_numero_perfecto import *
-from library.mod_numero_primo import *
+from library.mod_numero_par_o_impar import *
 
 def opciones():
     op = 0
@@ -11,6 +11,7 @@ def opciones():
 
         if(op < 1 or op > 4):
             print("Por favor elija una opcion entre 1 y 4")
+
     if(op == 1): 
         punto1()    
     if(op == 2):
