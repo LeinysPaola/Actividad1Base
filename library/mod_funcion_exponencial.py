6e9efe6f80480fdcb364edbf9497322c263a4056
Yilberth <yilberthgalarza444@gmail.com>
2021-09-09 18:01:35 -0500

    Yilberth

--- library/mod_funcion_exponencial.py
+++ library/mod_funcion_exponencial.py
@@ -1,38 +1,3 @@
-<<<<<<< Updated upstream
-#OPCION 444
-def factorial(num):
-    i = 0
-    valor = 1
-    for i in range(1, num +1):
-        valor = valor * i
-    return valor
-
-
-
-
-
-def punto4():
-
-    print("Para calcular la funcion exponencial se tomara n = 50 como constante")
-    numero = int(input("Escriba cualquier numero para darle valor a x: "))
-
-    valor = 0;
-    for i in range(1,51):
-
-        if(i == 1):
-            contenedor = 1 + numero
-        if(i > 1):
-            contenedor = contenedor + ((numero**i)/(factorial(i)))
-            
-    
-    return print("Respuesta: ", contenedor)
-
-
-
-        
-
-        
-=======
 #OPCION 4
 def factorial(num):
     i = 0
@@ -57,11 +22,4 @@ def punto4():
             contenedor = contenedor + ((numero**i)/(factorial(i)))
             
     
-    return print("Contiene final: ", contenedor)
-
-
-
-        
-
-        
->>>>>>> Stashed changes
+    return print("Respuesta: ", contenedor)
