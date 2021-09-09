3da594ff65dd1173c3df1010436d8d7837b02cce
Yilberth <yilberthgalarza444@gmail.com>
2021-09-09 18:36:45 -0500

    Yilberth

--- library/mod_numero_primo.py
+++ library/mod_numero_primo.py
@@ -7,11 +7,8 @@ def punto2():
 
     while(i <= numero):
         if(numero%i == 0):
-            print(i)
             comprobar = comprobar + 1
         i = i +1
-
-    print(comprobar)
 
     if comprobar <= 2:
         return print("Este numero es primo!!")
